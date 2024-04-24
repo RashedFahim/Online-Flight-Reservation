d9214c88f3c1e37872f9327ca15984a025ba0dac
RashedFahim <167600600+RashedFahim@users.noreply.github.com>
2024-04-24 21:44:55 +0600

    Update home.php

--- home.php
+++ home.php
@@ -201,7 +201,7 @@
         <ul>
             <li><a href="home.php">Home</a></li>
             <li><a href="about.php">About</a></li>
-            <li><a href="#">Services</a></li>
+            <li><a href="services.php">Services</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
     </nav>
